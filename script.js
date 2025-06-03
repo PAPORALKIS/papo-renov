7675b10cb4d82dc9953b80926f450908c18a3aeb
PAPORALKIS <k.paporalkis@gmail.com>
2025-06-03 21:07:43 +0200

    test position manu

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function getResponsivePlaneSize () {
 function updateCameraDistance (radius) {
   const fov      = THREE.MathUtils.degToRad(camera.fov);
   const distance = radius / Math.tan(fov / 2) * 1.5; // 10 % de marge visuelle
-  camera.position.set(0, 0, distance);
+  camera.position.set(0, 0, 80);
   return distance; // utile si on veut ré‑utiliser la valeur
 }
 
